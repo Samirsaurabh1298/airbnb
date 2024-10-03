e7f369b5fc13fcfa58050e8994044677fe99870a
Samir saurabh <samirverma527@gmail.com>
2024-10-03 08:54:04 +0530

    routes changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Home from "./components/home/Home";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchPage from "./components/searchPage/SearchPage";
 
 function App() {
